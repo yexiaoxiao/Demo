f40f879a1d72a08855f1877fb0a2c9dd146e7375
yexiaoxiao <773264939@qq.com>
2016-09-12 11:13:25 +0800

    忘了

---
+++ react/2.Demo/demo3router/16-huge-apps/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './app.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  }
+}
